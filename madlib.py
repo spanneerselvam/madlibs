e5d1f72073ce5bafb21405d8a3fe2d120c3bf42f
Saigopini Panneerselvam <spanneerselvam@scu.edu>
2017-07-18 21:41:53 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,5 @@
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+print("Your Madlib:")
+print("The", adjective, noun, verb, "over the lazy brown dog.")
